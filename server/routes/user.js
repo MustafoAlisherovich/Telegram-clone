f8210d98013cfce69d82a941ea927778664d1fcc
MustafoAlisherovich <mustafoalisherovic@gmail.com>
2026-01-24 19:34:46 +0300

    feat: extend user controller functionality

--- server/routes/user.js
+++ server/routes/user.js
@@ -3,7 +3,19 @@ const userController = require('../controllers/user.controller')
 const router = require('express').Router()
 
 router.get('/messages/:contactId', userController.getMessages)
+router.get('/contacts', userController.getContacts)
 
-router.post('/create-message', userController.createMessage)
+router.post('/message', userController.createMessage)
+router.post('/contact', userController.createContact)
+router.post('/reaction', userController.createReaction)
+router.post('/send-otp', userController.sendOtp)
+router.post('/message-read', userController.messageRead)
+
+router.put('/message/:messageId', userController.updateMessage)
+router.put('/profile', userController.updateProfile)
+router.put('/email', userController.updateEmail)
+
+router.delete('/message/:messageId', userController.deleteMessage)
+router.delete('/', userController.deleteUser)
 
 module.exports = router
